5cba0deaa95f6b040a85083751281d4e83619601
masteryourself <renzhao.ruan@foxmail.com>
2020-04-11 17:38:49 +0800

    mybatis 使用 log4j

--- tutorial-mybatis/tutorial-mybatis-log/src/main/java/pers/masteryourself/tutorial/orm/mybatis/log/LogApplication.java
+++ tutorial-mybatis/tutorial-mybatis-log/src/main/java/pers/masteryourself/tutorial/orm/mybatis/log/LogApplication.java
@@ -1,5 +1,6 @@
 package pers.masteryourself.tutorial.orm.mybatis.log;
 
+import org.apache.ibatis.logging.LogFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import pers.masteryourself.tutorial.orm.mybatis.log.config.SpringConfig;
 import pers.masteryourself.tutorial.orm.mybatis.log.domain.User;
@@ -15,6 +16,11 @@ import pers.masteryourself.tutorial.orm.mybatis.log.service.UserService;
  * @date : 2020/4/11 16:07
  */
 public class LogApplication {
+
+    static {
+        // 必须要在调用 MyBatis 任何方法之前调用这个方法，更改日志实现
+        LogFactory.useLog4JLogging();
+    }
 
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
